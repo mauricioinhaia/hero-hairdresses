26403a009d07c5f9d5be5385815fad63f2dc6928
Mauricio Inhaia <mauricio.inhaia@hotmail.com>
2023-06-01 23:41:52 -0300

    AWS Configs

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,5 +1,6 @@
 import { Router, request, response } from "express";
 import { UsersController } from "../controllers/UsersController";
+import { upload } from "../config/multer";
 class UsersRoutes {
   private router: Router;
   private usersController: UsersController;
@@ -16,6 +17,7 @@ class UsersRoutes {
 
     this.router.put(
       "/",
+      upload.single('avatar_url'),
       this.usersController.update.bind(this.usersController)
     );
 
